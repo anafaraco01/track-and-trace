ef932af1b80d2fcb760b22544118a9d6fd8fc0c0
spad0001 <spad0001@hz.nl>
2023-07-06 21:09:13 +0200

    Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CoordinatesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,10 +22,13 @@ Route::get('/app', function () {
     return view('app');
 });
 
+Route::resource("/coordinates", CoordinatesController::class);
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
+
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
