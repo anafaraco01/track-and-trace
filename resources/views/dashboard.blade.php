868f371289de29a166ad05c9fd17d223b334baf3
spad0001 <spad0001@hz.nl>
2023-07-07 05:49:57 +0200

    just increase the size of the table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,44 +1,56 @@
-<x-app-layout>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <x-app-layout>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <!-- resources/views/vehicles/index.blade.php -->
+        <style>
+            .table-bordered th,
+            .table-bordered td {
+                border-right: 1px solid #dbdbdb;
+            }
+        </style>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <!-- resources/views/vehicles/index.blade.php -->
 
-                @section('content')
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-                    <section class="hero  is-small  is-bold" style="background-color: cornflowerblue">
-                        <div class="hero-body">
-                            <div class="container">
-                                <p class="title is-2">Coordinates</p>
-                                <p class="subtitle is-3">Set your coordinates</p>
+                    @section('content')
+                        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+                        <section class="hero is-small is-bold" style="background-color: cornflowerblue">
+                            <div class="hero-body">
+                                <div class="container">
+                                    <p class="title is-2">Coordinates</p>
+                                    <p class="subtitle is-3">Set your coordinates</p>
+                                </div>
+                            </div>
+                        </section>
+                        <br>
+                        <div class="container">
+                            <div class="columns is-centered">
+                                <div class="column is-12">
+                                    <table class="table rounded is-fullwidth is-size-4">
+                                        <thead>
+                                        <tr>
+                                            <th>Place</th>
+                                            <th>Y Coordinate</th>
+                                            <th>X Coordinate</th>
+                                            <th>Permission</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($coordinates as $coordinate)
+                                            <tr class="{{ $coordinate->permission === 'Allowed' ? 'bg-green-500' : ($coordinate->permission === 'Forbidden' ? 'bg-red-500' : ($coordinate->permission === 'Conditional' ? 'bg-blue-400' : '')) }}">
+                                                <td>{{ $coordinate->place }}</td>
+                                                <td>{{ $coordinate->y_coordinate }}</td>
+                                                <td>{{ $coordinate->x_coordinate }}</td>
+                                                <td>{{ $coordinate->permission }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
-                    </section>
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th>Place</th>
-                            <th>Y Coordinate</th>
-                            <th>X Coordinate</th>
-                            <th>Permission</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($coordinates as $coordinate)
-                            <tr bgcolor="{{ $coordinate->permission === 'Allowed' ? 'lightgreen' : ($coordinate->permission === 'Forbidden' ? 'red' : ($coordinate->permission === 'Conditional' ? 'lightblue': '')) }}">
-
-                                <td>{{ $coordinate->place }}</td>
-                                <td>{{ $coordinate->y_coordinate }}</td>
-                                <td>{{ $coordinate->x_coordinate }}</td>
-                                <td>{{ $coordinate->permission }}</td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                @endsection
+                    @endsection
+                </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    </x-app-layout>
